3e96714d9e85d79729eac5c2ebfb8122c0235dc2
file-get-contents <ymstmsy2@gmail.com>
2023-10-10 04:25:08 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -64,49 +64,53 @@ function App() {
 
 
     return (
-        <dl>
-            <dt>RAM (GB)</dt>
-            <dd>{deviceMemory}</dd>
+        <article>
+            <header>
+                <h1>Navigator's Infomation</h1>
+                <p>The information contained in the navigator is displayed.</p>
+                <p>Some of the information is experimental and may not be correct.</p>
+                <p>Please note that some browsers may not support this information.</p>
+            </header>
+            <section>
+                <h2>Spec</h2>
+                <dl>
+                    <dt>RAM (GB)</dt>
+                    <dd>{deviceMemory}</dd>
 
-            <dt>CPU Threads (pieces)</dt>
-            <dd>{hardwareConcurrency}</dd>
+                    <dt>CPU Threads (pieces)</dt>
+                    <dd>{hardwareConcurrency}</dd>
 
-            <dt>ROM Usage/Quota (bytes)</dt>
-            <dd>{`${g[0]}/${g[1]}`}</dd>
+                    <dt>ROM Usage/Quota (bytes)</dt>
+                    <dd>{`${g[0]}/${g[1]}`}</dd>
 
-            <dt>OS</dt>
-            <dd>{userAgentData.platform}</dd>
+                    <dt>OS</dt>
+                    <dd>{userAgentData.platform}</dd>
 
-            <dt>Mobile</dt>
-            <dd>{userAgentData.mobile == 'unknown' ? 'unknown' : userAgentData.mobile ? 'Yes' : 'No'}</dd>
+                    <dt>Mobile</dt>
+                    <dd>{userAgentData.mobile == 'unknown' ? 'unknown' : userAgentData.mobile ? 'Yes' : 'No'}</dd>
 
-            <dt>Browser</dt>
-            <dd>{`${userAgentData.brands[0].brand} ${userAgentData.brands[0].version}`}</dd>
+                    <dt>Browser</dt>
+                    <dd>{`${userAgentData.brands[0].brand} ${userAgentData.brands[0].version}`}</dd>
 
-            
+                    <dt>Max Touch Points</dt>
+                    <dd>{maxTouchPoints}</dd>
 
+                    <dt>ONLINE</dt>
+                    <dd>{onLine == 'unknown' ? 'unknown' : onLine ? 'Yes' : 'No'}</dd>
 
-            <dt>Max Touch Points</dt>
-            <dd>{maxTouchPoints}</dd>
+                    <dt>Network (Mbps)</dt>
+                    <dd>{connection.downlink}</dd>
 
-            <dt>ONLINE</dt>
-            <dd>{onLine == 'unknown' ? 'unknown' : onLine ? 'Yes' : 'No'}</dd>
+                    <dt>Network Type</dt>
+                    <dd>{connection.effectiveType}</dd>
 
-            <dt>Network (Mbps)</dt>
-            <dd>{connection.downlink}</dd>
+                    <dt>Network RTT</dt>
+                    <dd>{connection.rtt}</dd>
 
-            <dt>Network Type</dt>
-            <dd>{connection.effectiveType}</dd>
+                </dl>
+            </section>
+        </article>
 
-            <dt>Network RTT</dt>
-            <dd>{connection.rtt}</dd>
-
-
-             
-
-        
-        
-        </dl>
     )
 }
 
